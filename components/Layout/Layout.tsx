98f6f40809eb04c39a312c15ce574ca7d15b3d44
Ray Molla <raymolla7@gmail.com>
2023-05-31 21:24:34 -0400

    fix screen height issue

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -63,13 +63,7 @@ const Layout = ({
           visibleRightSidebar={visibleRightSidebar}
           smallSidebar={smallSidebar}
         />
-        <div
-          className={`flex py-6 md:py-0 ${
-            hideRightSidebar
-              ? "min-h-screen min-h-screen-ios"
-              : "h-screen h-screen-ios"
-          }`}
-        >
+        <div className={"flex py-6 md:py-0 h-screen h-screen-ios"}>
           <div
             className={`relative flex grow max-w-full bg-n-1 rounded-[1.25rem] md:rounded-none dark:bg-n-6 ${
               !hideRightSidebar && "pr-[22.5rem] 2xl:pr-80 lg:pr-0"
