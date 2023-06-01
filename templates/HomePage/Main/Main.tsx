5c0245b47189a3261aa11896a8f3cf960b2256a9
Ray Molla <raymolla7@gmail.com>
2023-05-31 23:15:18 -0400

    alter home

--- templates/HomePage/Main/Main.tsx
+++ templates/HomePage/Main/Main.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Message from "@/components/Message/Message";
 import Menu from "@/components/Menu/Menu";
 
 import { navigation } from "@/constants/navigation";
@@ -7,25 +6,24 @@ import { navigation } from "@/constants/navigation";
 type MainProps = {};
 
 const Main = ({}: MainProps) => {
-  const [message, setMessage] = useState<string>("");
-
   return (
     <>
       <div className="grow px-10 py-20 overflow-y-auto scroll-smooth scrollbar-none 2xl:py-12 md:px-4 md:pt-0 md:pb-6">
         <div className="mb-10 text-center">
           <div className="h3 leading-[4rem] 2xl:mb-2 2xl:h4">
-            Unlock the power of AI
+            AI Gaming Companion
           </div>
           <div className="body1 text-n-4 2xl:body1S">
-            Chat with the smartest AI - Experience the power of AI with us
+            Chat with the first ever AI trained on games!
           </div>
         </div>
         <Menu className="max-w-[30.75rem] mx-auto" items={navigation} />
+        <div className="mt-10 text-center">
+          <div className="body1 text-n-4 2xl:body1S">
+            Vote on the next game!
+          </div>
+        </div>
       </div>
-      <Message
-        value={message}
-        onChange={(e: any) => setMessage(e.target.value)}
-      />
     </>
   );
 };
