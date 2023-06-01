a3a4dd35cf06b57bf6780c6979608dafb7858fae
Ray Molla <raymolla7@gmail.com>
2023-06-01 00:15:56 -0400

    add game info

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import Icon from "@/components/Icon/Icon";
+import Image from "@/components/Image/Image";
 
 type MenuType = {
   title: string;
-  icon: string;
-  color: string;
+  image: string;
   url: string;
+  enabled: boolean;
 };
 
 type MenuProps = {
@@ -14,27 +14,35 @@ type MenuProps = {
 };
 
 const Menu = ({ className, items }: MenuProps) => (
-  <div className={className}>
+  <div
+    className={`flex md:flex-col items-center justify-center gap-x-2 ${className}`}
+  >
     {items.map((item, index) => (
       <Link
-        className="group flex items-center mb-5 p-3.5 border border-n-3 rounded-xl h6 transition-all hover:border-transparent hover:shadow-[0_0_1rem_0.25rem_rgba(0,0,0,0.04),0px_2rem_1.5rem_-1rem_rgba(0,0,0,0.12)] last:mb-0 2xl:p-2.5 lg:p-3.5 dark:border-n-5 dark:hover:border-n-7 dark:hover:bg-n-7"
+        className={`group flex flex-col items-center mx-1 border-2 border-n-3 rounded-xl h6 transition-all hover:shadow-[0_0_1rem_0.25rem_rgba(0,0,0,0.04),0px_2rem_1.5rem_-1rem_rgba(0,0,0,0.12)] last:mb-0 2xl:p-2.5 lg:p-3.5 dark:border-n-5 dark:hover:border-n-7 dark:hover:bg-n-7 ${
+          item.enabled ? "pointer-events-auto" : "pointer-events-none"
+        }`}
         href={item.url}
         key={index}
       >
-        <div className="relative flex justify-center items-center w-15 h-15 mr-6">
-          <div
-            className="absolute inset-0 opacity-20 rounded-xl"
-            style={{
-              backgroundColor: item.color,
-            }}
-          ></div>
-          <Icon className="relative z-1" fill={item.color} name={item.icon} />
+        <div
+          className={`relative flex justify-center items-center w-38 h-52 ${
+            item.enabled ? null : ""
+          }`}
+        >
+          <Image
+            className="rounded-lg"
+            src={`/gamecovers/` + item.image}
+            fill
+            alt="Avatar"
+          />
+          {!item.enabled && (
+            <div className="absolute text-white text-3xl text-center bg-black bg-opacity-40">
+              Coming Soon
+            </div>
+          )}
         </div>
-        {item.title}
-        <Icon
-          className="ml-auto fill-n-4 transition-colors group-hover:fill-n-7 dark:group-hover:fill-n-4"
-          name="arrow-next"
-        />
+        <div className="text-center text-base mt-3">{item.title}</div>
       </Link>
     ))}
   </div>
