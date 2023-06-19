3a4f0e35789d308d7ad053769aa8bcf481b47efc
Ray Molla <raymolla7@gmail.com>
2023-06-19 10:36:17 -0400

    faq page refactor

--- pages-lib/UpdatesAndFaqPage/Faq/index.tsx
+++ pages-lib/UpdatesAndFaqPage/Faq/index.tsx
@@ -19,14 +19,6 @@ const Faq = ({ items }: FaqProps) => (
 				<FaqItem item={x} key={x.id} />
 			))}
 		</div>
-		<div className="mt-12 p-20 bg-n-2/50 rounded-[1.25rem] text-center md:py-16 md:px-8 dark:bg-n-7/50">
-			<div className="w-28 mx-auto mb-8">
-				<Image src="/images/faq-image.svg" width={112} height={112} alt="" />
-			</div>
-			<div className="mb-1 h5">Can’t find any answer?</div>
-			<div className="mb-8 base1 text-n-4">Let’s ask the smartest AI Chat</div>
-			<button className="btn-blue">Ask GameGuideAI</button>
-		</div>
 	</>
 )
 
