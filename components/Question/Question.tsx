1c800cd21a17def082b4db49a0461e6b1ee29766
Ray Molla <raymolla7@gmail.com>
2023-06-27 19:15:30 -0400

    change rounding

--- components/Question/Question.tsx
+++ components/Question/Question.tsx
@@ -10,7 +10,7 @@ type QuestionProps = {
 
 const Question = ({ content, image, document, time }: QuestionProps) => (
 	<div className="max-w-[50rem] ml-auto">
-		<div className="space-y-6 pt-6 px-6 pb-6 border-3 border-n-2 rounded-[1.25rem] md:p-5 md:pb-8 dark:border-transparent dark:bg-n-5/50">
+		<div className="space-y-6 pt-6 px-6 pb-6 border-3 border-n-2 rounded-[0.75rem] md:p-5 md:pb-8 dark:border-transparent dark:bg-n-5/50">
 			{document && <Document value={document} />}
 			<div className="">{content}</div>
 			{image && (
