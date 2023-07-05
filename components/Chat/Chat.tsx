24d3010eda1365ea52e8e3434ef5a7d2495775d5
Ray Molla <raymolla7@gmail.com>
2023-07-05 13:13:41 -0400

    fix padding

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -63,7 +63,7 @@ const Chat = ({ title, children, deleteAllMessages, clean }: ChatProps) => {
 				</div>
 			</div>
 			<div className="h-full overflow-y-scroll">
-				<div className="h-fit relative z-2 grow p-10 space-y-6 overflow-y-auto scroll-smooth scrollbar-none 2xl:p-6 md:p-5 mx-auto max-w-4xl w-full">
+				<div className="h-fit relative z-2 grow p-10 space-y-6 overflow-y-auto scroll-smooth scrollbar-none 2xl:p-8 md:p-5 mx-auto max-w-4xl w-full">
 					{children}
 				</div>
 			</div>
