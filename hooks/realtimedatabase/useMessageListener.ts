c0ad0021e95f095686aed7dbc290e2f2f851c72e
Istvan Kreisz <kreiszdev@gmail.com>
2023-07-05 23:59:21 +0200

    optimization

--- hooks/realtimedatabase/useMessageListener.ts
+++ hooks/realtimedatabase/useMessageListener.ts
@@ -5,6 +5,17 @@ import { useCallback, useEffect, useRef } from 'react'
 export function useMessageListener() {
 	const unsubscribe = useRef<Unsubscribe | null>()
 
+	const chunks = useRef<string[]>([])
+	const timer = useRef<NodeJS.Timer | undefined>()
+
+	const stopListener = useCallback(() => {
+		unsubscribe.current?.()
+		if (timer.current) {
+			clearInterval(timer.current)
+		}
+		chunks.current = []
+	}, [])
+
 	const listenToMessageUpdates = useCallback(
 		(
 			userId: string,
@@ -14,24 +25,31 @@ export function useMessageListener() {
 		) => {
 			unsubscribe.current?.()
 
+			stopListener()
+			let index = 0
+			timer.current = setInterval(() => {
+				if (chunks.current.length > index) {
+					updated(chunks.current[index])
+					index++
+				}
+			}, 30)
 			const messageRef = ref(database, [userId, conversationId, messageId].join('/'))
 			unsubscribe.current = onValue(messageRef, (snapshot) => {
 				let val = snapshot.val()
 				if (val && typeof val === 'string' && val.length) {
-					updated(val)
+					const chunked = val.split(' ').map((chunk, index, array) => {
+						return array.length - 1 === index ? chunk : chunk + ' '
+					})
+					chunks.current.push(...chunked)
 				}
 			})
 		},
-		[]
+		[stopListener]
 	)
-
-	const stopListener = useCallback(() => {
-		unsubscribe.current?.()
-	}, [])
 
 	useEffect(() => {
 		return () => {
-			unsubscribe.current?.()
+			stopListener()
 		}
 	}, [])
 
