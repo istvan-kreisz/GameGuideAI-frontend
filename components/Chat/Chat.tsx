dc759dc36cde8fa4daa5918204545342be768e6e
Ray Molla <raymolla7@gmail.com>
2023-07-04 20:28:38 -0400

    fix scrolling

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -62,8 +62,10 @@ const Chat = ({ title, children, deleteAllMessages, clean }: ChatProps) => {
 					</button> */}
 				</div>
 			</div>
-			<div className="relative z-2 grow p-10 space-y-6 overflow-y-auto scroll-smooth scrollbar-none 2xl:p-6 md:p-5 mx-auto max-w-4xl w-full">
-				{children}
+			<div className="h-full overflow-y-scroll">
+				<div className="h-fit relative z-2 grow p-10 space-y-6 overflow-y-auto scroll-smooth scrollbar-none 2xl:p-6 md:p-5 mx-auto max-w-4xl w-full">
+					{children}
+				</div>
 			</div>
 		</>
 	)
