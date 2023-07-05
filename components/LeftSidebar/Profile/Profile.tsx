5b819ea9ffd25646ef3e231596078dabab770188
Ray Molla <raymolla7@gmail.com>
2023-07-04 20:37:27 -0400

    fix profile spacing

--- components/LeftSidebar/Profile/Profile.tsx
+++ components/LeftSidebar/Profile/Profile.tsx
@@ -12,7 +12,7 @@ const Profile = ({ visible }: ProfileProps) => {
 	return (
 		<div className={`${visible ? 'mb-6' : 'mb-3 shadow-[0_1.25rem_1.5rem_0_rgba(0,0,0,0.5)]'}`}>
 			<div className={`${!visible && 'p-2.5 bg-n-6 rounded-xl'}`}>
-				<div className={`flex items-center ${visible ? 'justify-center' : 'px-2.5 py-2.5 pb-4.5'}`}>
+				<div className={`flex items-center ${visible ? 'justify-center' : 'px-2.5 py-2.5'}`}>
 					{!visible && (
 						<>
 							<div className="mr-4">
@@ -35,7 +35,7 @@ const Profile = ({ visible }: ProfileProps) => {
 				</div>
 				{!visible && (
 					<>
-						<div className="text-center px-7 pt-4 opacity-90">
+						<div className="text-center px-7 py-2 opacity-90">
 							{/*<Link className="btn-stroke-dark w-full mt-2" href="/pricing">
 						Support us on Patreon!
 						</Link>*/}
