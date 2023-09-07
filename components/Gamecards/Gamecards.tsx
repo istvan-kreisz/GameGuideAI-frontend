8614a024d1f4043e9d3b176d5054d58e2deddc9e
Ray Molla <raymolla7@gmail.com>
2023-09-07 12:07:56 -0400

    make cards not clickable

--- components/Gamecards/Gamecards.tsx
+++ components/Gamecards/Gamecards.tsx
@@ -15,6 +15,7 @@ type MenuProps = {
 	items: MenuType[]
 	filterEnabled?: boolean
 	filterDisabled?: boolean
+	clickable?: boolean
 }
 
 const Gamecards = ({
@@ -22,6 +23,7 @@ const Gamecards = ({
 	items,
 	filterEnabled = false,
 	filterDisabled = false,
+	clickable = true,
 }: MenuProps) => {
 	const [visible, setVisible] = useState<boolean>(false)
 
@@ -48,40 +50,77 @@ const Gamecards = ({
 				scrollbarWidth: 'thin',
 			}}
 		>
-			{filteredItems.map((item, index) => (
-				<Link href={item.url} key={index} passHref>
-					<div
-						className={`group flex flex-col items-center border-2 border-n-3 rounded-xl h6 transition-all hover:shadow-[0_0_1rem_0.25rem_rgba(0,0,0,0.04),0px_2rem_1.5rem_-1rem_rgba(0,0,0,0.12)] last:mb-0 dark:border-n-4 dark:hover:border-n-5 dark:hover:bg-surf-1`}
-						onClick={(event) => handleClick(event, item.enabled)}
-					>
+			{filteredItems.map((item, index) => {
+				if (clickable) {
+					return (
+						<Link href={item.url} key={index} passHref>
+							<div
+								className={`group flex flex-col items-center border-2 border-n-3 rounded-xl h6 transition-all hover:shadow-[0_0_1rem_0.25rem_rgba(0,0,0,0.04),0px_2rem_1.5rem_-1rem_rgba(0,0,0,0.12)] last:mb-0 dark:border-n-4 dark:hover:border-n-5 dark:hover:bg-surf-1`}
+								onClick={(event) => handleClick(event, item.enabled)}
+							>
+								<div
+									className={`relative flex justify-center items-center w-[212px] h-72 2xl:w-[150px] 2xl:h-52 xl:w-[127px] xl:h-44 overflow-hidden`}
+								>
+									<Image
+										className="rounded-lg"
+										src={`/gamecovers/` + item.image}
+										fill
+										sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 10vw"
+										alt={`Game cover - ${item.title}`}
+										priority={index < 10}
+									/>
+									{!item.enabled && (
+										<div className="absolute text-white 2xl:text-2xl text-3xl text-center bg-black bg-opacity-40 w-full">
+											Vote On It!
+										</div>
+									)}
+									{item.enabled && (
+										<div className="absolute top-[4%] right-[-11%] 2xl:right-[-16%] xl:right-[-20%] rotate-45 bg-primary-1 text-white px-8 whitespace-nowrap text-center py-1 text-sm font-medium">
+											BETA
+										</div>
+									)}
+								</div>
+								<div className="text-center text-sm font-bold my-auto py-3 whitespace-nowrap">
+									{item.title}
+								</div>
+							</div>
+						</Link>
+					)
+				} else {
+					return (
 						<div
-							className={`relative flex justify-center items-center w-[212px] h-72 2xl:w-[150px] 2xl:h-52 xl:w-[127px] xl:h-44 overflow-hidden`}
+							className={`group flex flex-col items-center border-2 border-n-3 rounded-xl h6 transition-all hover:shadow-[0_0_1rem_0.25rem_rgba(0,0,0,0.04),0px_2rem_1.5rem_-1rem_rgba(0,0,0,0.12)] last:mb-0 dark:border-n-4 dark:hover:border-n-5 dark:hover:bg-surf-1`}
+							onClick={(event) => handleClick(event, item.enabled)}
 						>
-							<Image
-								className="rounded-lg"
-								src={`/gamecovers/` + item.image}
-								fill
-								sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 10vw"
-								alt={`Game cover - ${item.title}`}
-								priority={index < 10}
-							/>
-							{!item.enabled && (
-								<div className="absolute text-white 2xl:text-2xl text-3xl text-center bg-black bg-opacity-40 w-full">
-									Vote On It!
-								</div>
-							)}
-							{item.enabled && (
-								<div className="absolute top-[4%] right-[-11%] 2xl:right-[-16%] xl:right-[-20%] rotate-45 bg-primary-1 text-white px-8 whitespace-nowrap text-center py-1 text-sm font-medium">
-									BETA
-								</div>
-							)}
+							<div
+								className={`relative flex justify-center items-center w-[212px] h-72 2xl:w-[150px] 2xl:h-52 xl:w-[127px] xl:h-44 overflow-hidden`}
+							>
+								<Image
+									className="rounded-lg"
+									src={`/gamecovers/` + item.image}
+									fill
+									sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 10vw"
+									alt={`Game cover - ${item.title}`}
+									priority={index < 10}
+								/>
+								{!item.enabled && (
+									<div className="absolute text-white 2xl:text-2xl text-3xl text-center bg-black bg-opacity-40 w-full">
+										Vote On It!
+									</div>
+								)}
+								{item.enabled && (
+									<div className="absolute top-[4%] right-[-11%] 2xl:right-[-16%] xl:right-[-20%] rotate-45 bg-primary-1 text-white px-8 whitespace-nowrap text-center py-1 text-sm font-medium">
+										BETA
+									</div>
+								)}
+							</div>
+							<div className="text-center text-sm font-bold my-auto py-3 whitespace-nowrap">
+								{item.title}
+							</div>
 						</div>
-						<div className="text-center text-sm font-bold my-auto py-3 whitespace-nowrap">
-							{item.title}
-						</div>
-					</div>
-				</Link>
-			))}
+					)
+				}
+			})}
 			<Modal video visible={visible} onClose={() => setVisible(false)}>
 				<iframe
 					style={{ width: 'inherit', height: '100%' }}
