37c9e35031250ea7f137bbc71ff72498b2484c7c
Ray Molla <raymolla7@gmail.com>
2023-09-18 20:11:53 -0400

    added ms clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,6 +30,17 @@ export default function Document() {
 
 				<meta name="msapplication-TileColor" content="#da532c" />
 				<meta name="theme-color" content="#ffffff" />
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(c,l,a,r,i,t,y){
+								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "script", "ixbrwch8eu");
+						`,
+					}}
+				/>
 			</Head>
 			<body>
 				<Main />
