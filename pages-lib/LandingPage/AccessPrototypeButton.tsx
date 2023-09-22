811c0ed9f44b958427926b671db9a6b3e7d5c606
Ray Molla <raymolla7@gmail.com>
2023-09-21 22:23:03 -0400

    fix

--- pages-lib/LandingPage/AccessPrototypeButton.tsx
+++ pages-lib/LandingPage/AccessPrototypeButton.tsx
@@ -5,7 +5,7 @@ const discordButton = () => {
 			aria-label="Discord"
 			className="text-white bg-gradient-to-br from-purple-600 to-primary-1 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center  hover:bg-blue-700 flex flex-row gap-x-2 justify-center items-center w-5/6 max-w-xs h-12 mb-14 mt-5 mx-auto"
 		>
-			<p className="text-3xl lg:text-2xl font-medium text-white">Access the Prototype</p>
+			<p className="text-2xl font-medium text-white">Access the Prototype</p>
 		</a>
 	)
 }
